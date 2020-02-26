03f0187f97b04581f1287e221c4b3e1a3899c15f
Kitokitola <tetiana.la21@gmail.com>
2020-02-27 01:20:23 +0200

    homework #7 added

---
+++ 7-hw-closures/script.js
@@ -0,0 +1,56 @@
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
+const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+
+// Task 1
+
+const getMyTaxes = function(salary) {
+  return salary * this.tax;
+};
+
+console.log(getMyTaxes.call(ukraine, 1500));
+
+
+
+// Task 2
+
+const getMiddleTaxes = function() {
+  return this.tax * this.middleSalary;
+};
+
+console.log(getMiddleTaxes.call(latvia));
+
+
+
+// Task 3
+
+const getTotalTaxes = function() {
+  return this.tax * this.middleSalary * this.vacancies;
+};
+
+console.log(getTotalTaxes.call(litva));
+
+
+
+// Task 4
+
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+};
+
+const getMySalary = function(country) {
+  const salary = getRandomNumber(1500, 2000);
+  const taxes = getMyTaxes.call(country, salary);
+  const profit = salary - taxes;
+  const InfoObj = {
+    salary,
+    taxes,
+    profit
+  };
+  return setInterval(() => {
+    console.log(InfoObj);
+  }, 10000);
+};
+
+getMySalary(latvia);
