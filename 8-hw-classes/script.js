7bb53e7f8b28a61f89428dbc3a68c15ef52c9240
Kitokitola <tetiana.la21@gmail.com>
2020-03-01 15:50:07 +0200

    homework #8 added

--- 8-hw-classes/script.js
+++ 8-hw-classes/script.js
@@ -1,3 +1,5 @@
+// Main task
+
 class Student {
   constructor(university, course, fullName) {
     this.university = university;
@@ -8,26 +10,23 @@ class Student {
   }
 
   getInfo() {
-    return `Student of ${this.course} of ${this.university}, this.fullName`;
+    return `${this.course} year student of ${this.university}, ${this.fullName}`;
   }
 
-  get marks() {
-    if (isDismissed) {
+  get studentMarks() {
+    if (this.isDismissed) {
       return null;
     } else {
       return this.marks;
     }
   }
 
-  set marks(mark) {
-    this.marks = this.marks.push(mark);
+  set studentMarks(mark) {
+    this.marks.push(mark);
   }
 
   getAverageMark() {
-    const markSum = this.marks.reduce(
-      (accumulator, mark) => accumulator + mark,
-      0
-    );
+    const markSum = this.marks.reduce((accumulator, mark) => accumulator + mark, 0);
     return markSum / this.marks.length;
   }
 
@@ -40,14 +39,43 @@ class Student {
   }
 }
 
-// Advanced
+const ostapStudent = new Student('Lviv Medical University', 'First', 'Ostap Bender');
+
+console.log(ostapStudent.getInfo());
+console.log(ostapStudent.studentMarks);
+
+ostapStudent.studentMarks = 5;
+console.log(ostapStudent.studentMarks);
+
+console.log(ostapStudent.getAverageMark());
+
+ostapStudent.dismiss();
+console.log(ostapStudent.isDismissed);
+console.log(ostapStudent.studentMarks);
+
+ostapStudent.recover();
+console.log(ostapStudent.isDismissed);
+
+
+
+// Advanced task
 
 class BudgetStudent extends Student {
   constructor(university, course, fullName) {
     super(university, course, fullName);
+    setInterval(() => this.getScholarship(), 30000);
   }
 
   getScholarship() {
-    
+    if (this.getAverageMark() >= 4 && !this.isDismissed) {
+      console.log("You've received 1400 grn scholarship");
+    } else {
+      console.log("You haven't received a scholarship");
+    }
   }
 }
+
+const ihorStudent = new BudgetStudent('Lviv Polytechnic University', 'Second', 'Ihor Kulchytskiy');
+
+console.log(ihorStudent.getInfo());
+console.log(ihorStudent.studentMarks);
