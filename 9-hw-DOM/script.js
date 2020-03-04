3ab55581fb9cd8df4dfb464abbe4e55d7f41913b
Kitokitola <tetiana.la21@gmail.com>
2020-03-04 18:32:57 +0200

    homework #9 added

---
+++ 9-hw-DOM/script.js
@@ -0,0 +1,32 @@
+// Main task
+
+const getRandomColor = () => {
+  let maxColor = 255;
+  return `rgb(${Math.floor(Math.random() * maxColor+1)}, ${Math.floor(Math.random() * maxColor+1)}, ${Math.floor(Math.random() * maxColor+1)})`;
+}
+
+
+const generateBlocks = () => {
+  const mainBlock = document.getElementById('main-block');
+  const blocksAmount = 25;
+  mainBlock.innerHTML = '';
+  for (i = 0; i < blocksAmount; i++) {
+    let coloredBlock = document.createElement('div');
+    coloredBlock.style.width = '50px';
+    coloredBlock.style.height = '50px';
+    coloredBlock.style.backgroundColor = getRandomColor();
+    mainBlock.append(coloredBlock);
+  }
+
+  return mainBlock;
+}
+
+
+// Advanced task
+
+const generateBlocksInterval = () => {
+    setInterval(() => generateBlocks(), 1000);
+}
+
+generateBlocksInterval()
+
