e4e5477a116929f99f890f01596e482a6e8a442e
Kitokitola <tetiana.la21@gmail.com>
2020-03-15 16:52:09 +0200

    final changes

--- 11-hw-promises/script.js
+++ 11-hw-promises/script.js
@@ -1,14 +1,4 @@
-const generateChineseChar = (length) => {
-    const time = 50;
-    return new Promise((resolve, reject) => {
-setTimeout(() => {
-    const sign = Date.now().toString().slice(-5);
-    resolve(String.fromCharCode(sign))
-    }, time*length)
-    })
-    }
-            
-      console.log(generateChineseChar(100));
+
 
 
 
