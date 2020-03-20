3aef8661648ded4238a3ff20599bba536ec4f2fc
Kitokitola <tetiana.la21@gmail.com>
2020-03-20 20:52:31 +0200

    homework #11 added

--- 11-hw-promises/script.js
+++ 11-hw-promises/script.js
@@ -1,5 +1,33 @@
+async function getRandomChinese(length) {
+  let str = '';
+  const ms = 50;
+  const time = ms * length;
+  console.log(`time: ${time}ms`);
+  try {
+    for (let i = 0; i < length; i++) {
+      let promise = new Promise(resolve => {
+        setTimeout(() => {
+          const sign = Date.now()
+            .toString()
+            .slice(-5);
+          const char = String.fromCharCode(sign);
+          resolve(char);
+        }, time);
+      });
+      await promise.then(letter => {
+        str += letter;
+      });
+    }
+    return str;
+  } catch (error) {
+    console.log(`Error!`);
+  }
+}
+
+
+getRandomChinese(6).then(str => {
+  console.log(str);
+});
 
 
 
-
-    
